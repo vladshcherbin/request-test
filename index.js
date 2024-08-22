3f60d5562d761496c3b7dd11a0876599d67f94b0
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-08-23 01:31:02 +0300

    Add koa server

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const ipResponse = await fetch('https://api.ipify.org?format=json', {
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  method: 'GET'
+import Koa from 'koa'
+
+const app = new Koa()
+
+app.use(async ctx => {
+  const ipResponse = await fetch('https://api.ipify.org?format=json', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'GET'
+  })
+  
+  const ipJson = await ipResponse.json()
+  
+  console.log('IP', ipJson)
+  
+  const brandsResponse = await fetch('https://api.av.by/offer-types/cars/catalog/brand-items', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'GET'
+  })
+  
+  console.log(brandsResponse)
+
+  ctx.body = 'Hello World'
 })
 
-const ipJson = await ipResponse.json()
-
-console.log('IP', ipJson)
-
-const brandsResponse = await fetch('https://api.av.by/offer-types/cars/catalog/brand-items', {
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  method: 'GET'
-})
-
-console.log(brandsResponse)
+app.listen(process.env.PORT)
